68e7e1fb573612726d042de318ea70a4bfef15e8
Anshul Aggarwal <anshulaggarw4l@gmail.com>
2023-07-25 14:44:17 -0700

    get rid of warning on Chat.eval

--- lib/emcee/chat.ex
+++ lib/emcee/chat.ex
@@ -2,8 +2,13 @@ defmodule Emcee.Chat do
   alias Emcee.Prompt
 
   @type message :: %{
-    required(:role) => String.t(),
-    required(:content) => String.t() | Prompt.t(),
-    optional(:name) => String.t()
-  }
+          required(:role) => String.t(),
+          required(:content) => String.t() | Prompt.t(),
+          optional(:name) => String.t()
+        }
+
+  @spec eval(t(), Keyword.t()) :: String.t()
+  def eval(%__MODULE__{template: template}, inputs) do
+    EEx.eval_string(template, inputs)
+  end
 end
